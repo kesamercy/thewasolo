c76e70dcf1352262846afe9545bee18c5120e713
Nekesa Mercy <kesamercy@gmail.com>
2020-06-17 14:53:54 -0400

    implementation to store the selections of the body parts and pass them to the php script in array format-debugging

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function () {
     const pieces = document.getElementsByTagName('svg');
-    
+    var selection = [];
     for (var i = 0; pieces.length; i++) {
-        var selection = [];
+        
         let _piece = pieces[i];
         _piece.onclick = function(t) {
             if (t.target.getAttribute('data-position') != null) {
@@ -22,17 +22,18 @@ window.onload = function () {
                 
             }
 
-            $.ajax({ 
-                type: "POST", 
-                url: "body-exam.php", 
-                data: { pain_points : selection}, 
-                success: function() { 
-                       alert("Success"); 
-                 } 
-         }); 
-
+            
             
         }
     }
+    $.ajax({ 
+        type: "POST", 
+        url: "body-exam.php", 
+        data: { pain_points : selection}, 
+        success: function() { 
+               alert("Success"); 
+         } 
+ }); 
+
     
 }
