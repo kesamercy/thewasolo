4c058bfcf942e13fdb1f30f8d4a3e8ad8f2b5a8c
Nekesa Mercy <kesamercy@gmail.com>
2020-06-19 15:20:12 -0400

    added a model implementation

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php
 session_start();
 include_once "access-db.php";
+
+
+if(isset($_POST['btn-login'])){
+
+  $email = $_POST["email"];
+  $paswd = $_POST["paswd"];
+
+  if(empty($email) || empty($paswd)){
+
+      echo 'fill in the blanks';
+  }
+  else {
+      $query = "SELECT * FROM farm_clients WHERE customer_email='$email'";
+      $result = mysqli_query($conn, $query);
+
+      if($row=mysqli_fetch_assoc($result)){
+          $db_password = $row['customer_password'];
+
+          if($paswd == $db_password){
+              $idnum=$row['customer_id'];
+              $_SESSION["user_id"] = $idnum;
+              
+              header("location:pat-lang.php?user_id=" .$idnum);
+          }
+          else {
+              echo 'incorrect password';
+             // header("location:customer-login.php");
+          }
+      }
+      else {
+          echo '<p>the user does not exist</p>';
+         // header("location:customer-login.php");
+      }
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -80,13 +115,13 @@ include_once "access-db.php";
                     <div class="modal-content">
                         <span class="close">&times;</span>
                         <div id="frm">
-                            <form method='POST' action="customer-login-auth.php">
+                            <form method='POST' action="doctor-login-auth.php">
 
                                 <input type="text" class="login_input" id="email" name="email" placeholder="Enter Email"
-                                    autofocus>
+                                    required>
                                 <br><br>
                                 <input type="text" class="login_input" id="paswd" name="paswd"
-                                    placeholder="Enter Password">
+                                    placeholder="Enter Password" required>
 
                                 <br><br>
                                 <div class="button">
