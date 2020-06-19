5f0136b1a3e0e23a9a60f138f3d4c19b6331e7fd
Nekesa Mercy <kesamercy@gmail.com>
2020-06-19 15:42:25 -0400

    modified the login authentication for the doctor

--- index.php
+++ index.php
@@ -2,39 +2,29 @@
 session_start();
 include_once "access-db.php";
 
+if (isset($_POST['btn-login'])) {
 
-if(isset($_POST['btn-login'])){
+    $email = $_POST["email"];
+    $paswd = $_POST["paswd"];
 
-  $email = $_POST["email"];
-  $paswd = $_POST["paswd"];
+    $query = "SELECT * FROM translator_doctors WHERE email='$email'";
+    $result = mysqli_query($conn, $query);
 
-  if(empty($email) || empty($paswd)){
+    if ($row = mysqli_fetch_assoc($result)) {
+        $db_password = $row['password'];
 
-      echo 'fill in the blanks';
-  }
-  else {
-      $query = "SELECT * FROM farm_clients WHERE customer_email='$email'";
-      $result = mysqli_query($conn, $query);
+        if ($paswd == $db_password) {
+            $idnum = $row['id'];
+            $_SESSION["user_id"] = $idnum;
 
-      if($row=mysqli_fetch_assoc($result)){
-          $db_password = $row['customer_password'];
-
-          if($paswd == $db_password){
-              $idnum=$row['customer_id'];
-              $_SESSION["user_id"] = $idnum;
-              
-              header("location:pat-lang.php?user_id=" .$idnum);
-          }
-          else {
-              echo 'incorrect password';
-             // header("location:customer-login.php");
-          }
-      }
-      else {
-          echo '<p>the user does not exist</p>';
-         // header("location:customer-login.php");
-      }
-  }
+            header("location:pat-lang.php?user_id=" . $idnum);
+        } else {
+            echo 'incorrect password';
+        }
+    } else {
+        echo '<p>the user does not exist</p>';
+        
+    }
 }
 ?>
 <!DOCTYPE html>
