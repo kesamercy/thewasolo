8260ff4931e7161064b4904d478b8fb1cf44868b
Nekesa Mercy <kesamercy@gmail.com>
2020-06-19 23:12:52 -0400

    changed the location of the js code for the modal button for login modal

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (isset($_POST['btn-login'])) {
             header("location:pat-lang.php?user_id=" . $idnum);
         } else {
             $_SESSION['message'] = 'Incorrect password';
-            
+
         }
     } else {
         $_SESSION['message'] = 'The user does not exist';
@@ -109,11 +109,11 @@ if (isset($_POST['btn-login'])) {
                         <br><br>
                         <div class="display-message">
                             <?php
-                              if (isset($_SESSION['message'])) {
-                                  echo $_SESSION['message'];
-                                  unset($_SESSION['message']);
-                              }
-                              ?>
+if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+?>
                         </div>
                         <div id="frm">
                             <form method='POST' action="">
@@ -238,12 +238,31 @@ if (isset($_POST['btn-login'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="index.js"></script>
     <script>
-    $(document).ready(function() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
 
-        displayTime();
-        setInterval(displayTime, 1000);
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
 
-    });
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
     </script>
 
 </body>
