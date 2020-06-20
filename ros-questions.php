794337ea58c595a2acdbcbf69b9f08a3bca26201
Nekesa Mercy <kesamercy@gmail.com>
2020-06-20 11:36:54 -0400

    changing the styling for ui elemtns

--- ros-questions.php
+++ ros-questions.php
@@ -2,13 +2,12 @@
 session_start();
 include_once "access-db.php";
 
-if(!isset($_SESSION["user_id"])){ //if login in session is not set
+if (!isset($_SESSION["user_id"])) { //if login in session is not set
     header("index.php");
 }
 
 $query = "SELECT * FROM translator_qtncategory";
 $query_results = $conn->query($query);
-
 
 if (count($_POST) > 0) {
 
@@ -17,11 +16,10 @@ if (count($_POST) > 0) {
     $lang_id = $_SESSION['language'];
     $category_id = $_POST['category'];
 
-    // this is the code responsible to show the questions for the chosen category 
+    // this is the code responsible to show the questions for the chosen category
     $sql = "SELECT * FROM translator_content WHERE language_id='$lang_id' AND category_id='$category_id'";
     $result = $conn->query($sql);
 
-    
 }
 
 ?>
@@ -62,135 +60,137 @@ if (count($_POST) > 0) {
 
     </div>
 
-    <p>Please select the body category for the questions you would like to ask the patient </p>
-    <!-- get all the questions from the chosen category and show them-->
-    <!-- you need the type as a hidden in the form so that you can return the selected category when the form is submitted.-->
-    <?php
+    <div class="page-content">
 
-        if ($query_results->num_rows > 0) { 
-            echo "<table class='prodcue-table'><tr style='height: 80px'><th style='text-align:left'> category </th></tr><br><br>";
-            echo "<tr style='height: 40px'>";
-            // output data of each row
-            while ($row = mysqli_fetch_array($query_results)) {
-                echo "<form method='post' action=''>";
-                $category = $row["cat_name"];
-                $category_id = $row["id"];
-                
-                echo "<td>" . $category . "</td>
-                            
+
+        <p>Please select the body category for the questions you would like to ask the patient </p>
+        <!-- get all the questions from the chosen category and show them-->
+        <!-- you need the type as a hidden in the form so that you can return the selected category when the form is submitted.-->
+        <?php
+
+if ($query_results->num_rows > 0) {
+    echo "<table class='prodcue-table'><tr style='height: 80px'><th style='text-align:left'> category </th></tr><br><br>";
+    echo "<tr style='height: 40px'>";
+    // output data of each row
+    while ($row = mysqli_fetch_array($query_results)) {
+        echo "<form method='post' action=''>";
+        $category = $row["cat_name"];
+        $category_id = $row["id"];
+
+        echo "<td>" . $category . "</td>
+
                             <input name='category'  type='hidden' value='$category_id' >
                             <td> <input type='submit' value='select'></td>";
-                        
-                        echo "</form>";
-            }
-            echo "</tr>";
-            echo "</table>";
 
-        } else {
-            echo "0 results for the category table";
-        }
-    ?>
+        echo "</form>";
+    }
+    echo "</tr>";
+    echo "</table>";
 
-    <?php
+} else {
+    echo "0 results for the category table";
+}
+?>
 
-    // <!-- select all the questions for the specified language and the category -->
-    if ($result->num_rows > 0) {
-    
-        // output data of each row
-        while ($row = mysqli_fetch_array($result)) {
+        <?php
 
-            echo "<form method='post' action='response-form.php'>";
+// <!-- select all the questions for the specified language and the category -->
+if ($result->num_rows > 0) {
 
-            $qtn_id = $row["question_id"];
-            $sql = "SELECT question FROM translator_questions WHERE id='$qtn_id'";
-            $result = $conn->query($sql);
-            $qtn = $row["question"];
+    // output data of each row
+    while ($row = mysqli_fetch_array($result)) {
 
-            //     <!-- get the first qtn from the results and display the text in english -->
-            echo "<h1> ".$qtn." </h1>";
+        echo "<form method='post' action='response-form.php'>";
 
-            // <!-- get the file path for that qtn to retrive the audio and show t -->
-            $audio_path = $row["sourcefile"];
-            echo "<img src='image.png' onclick='playAudio('mysound.mp3')'>";
+        $qtn_id = $row["question_id"];
+        $sql = "SELECT question FROM translator_questions WHERE id='$qtn_id'";
+        $result = $conn->query($sql);
+        $qtn = $row["question"];
 
-            echo "<audio controls>
+        //     <!-- get the first qtn from the results and display the text in english -->
+        echo "<h1> " . $qtn . " </h1>";
+
+        // <!-- get the file path for that qtn to retrive the audio and show t -->
+        $audio_path = $row["sourcefile"];
+        echo "<img src='image.png' onclick='playAudio('mysound.mp3')'>";
+
+        echo "<audio controls>
                 <source src='horse.ogg' type='audio/ogg'>
                 <source src='horse.mp3' type='audio/mpeg'>
             Your browser does not support the audio element.
             </audio>";
 
-            // you need this as a hidden feature so that you can return the selected value when the form is submitted.
-            //you do not need to submit the question since the patient will listen to that via audio
-            //you need to submit the cateogy so that the correct response form is displayed for the patient to enter their answer
-            echo "<input name='category'  type='hidden' value='$category_id' >";
+        // you need this as a hidden feature so that you can return the selected value when the form is submitted.
+        //you do not need to submit the question since the patient will listen to that via audio
+        //you need to submit the cateogy so that the correct response form is displayed for the patient to enter their answer
+        echo "<input name='category'  type='hidden' value='$category_id' >";
 
-            // <!-- get response button to submit the question for the patient to respond selected qtn and submit the form-->
-            echo "<input type='submit' name='response' value='get response'>";
-           
-            
-            echo "</form>";
-        }
+        // <!-- get response button to submit the question for the patient to respond selected qtn and submit the form-->
+        echo "<input type='submit' name='response' value='get response'>";
 
-    } else {
-        echo "0 results for the questions based on the language and the category";
+        echo "</form>";
     }
 
+} else {
+    echo "0 results for the questions based on the language and the category";
+}
 
-    ?>
+?>
 
-    <hr>
+        <hr>
 
-    <h1>Patient real-time response</h1>
+        <h1>Patient real-time response</h1>
 
-    <?php
+        <?php
 
-    if(isset($_POST['record-response'])){
-        $answer = $_POST["response"];
-        $question = $_POST["question"];
-        $patient_id = $_SESSION['patient_id'];
+if (isset($_POST['record-response'])) {
+    $answer = $_POST["response"];
+    $question = $_POST["question"];
+    $patient_id = $_SESSION['patient_id'];
 
-        // think about recording the session number too
+    // think about recording the session number too
 
-        // display the patient response for the doctor
-        echo "<p> Patient answer: " .$answer." </p>";
+    // display the patient response for the doctor
+    echo "<p> Patient answer: " . $answer . " </p>";
 
-        // store the response into the db for the paitent responses 
-        if (mysqli_query($conn, "INSERT INTO patient_table (patient_id, question, answer) VALUES ('$patient_id', '$question', '$answer')")) {
-            echo 'patient response has been recorded ';
+    // store the response into the db for the paitent responses
+    if (mysqli_query($conn, "INSERT INTO patient_table (patient_id, question, answer) VALUES ('$patient_id', '$question', '$answer')")) {
+        echo 'patient response has been recorded ';
 
-        } else {
-            echo 'there was an error with the system, patient response was not recorded';
-        }
+    } else {
+        echo 'there was an error with the system, patient response was not recorded';
+    }
 
+}
 
-    }  
-
-    ?>
-
-
-
-    <!-- show the doctor the response from the patient  -->
-
-    <!-- save the doctor question and the response to the response form table in the database -->
-    <!-- when the doctor clicks the end interview button, the retrive the data from the response form table -->
-
-    <!-- when that submit button is clicked, show the question on the parent portal   -->
-    <!-- have the form submit to patient.auth -->
-    <!-- the paitent auth should display the content that has been submitied and the paitnet should see the question -->
-    <!-- the paithent response form should show -->
-    <!-- the paitient response should be submitted and shown on the response section for the doctor -->
+?>
 
 
 
-    <!--the landing page for the paitent should be the page that asks for the code from the doctor to accept. that code should be saved as the session code  -->
-    <!-- so you update the session code so that the session for the patient and the doctor are the same -->
+        <!-- show the doctor the response from the patient  -->
 
-    <!-- if the doctor clicks next category, then retrive the questions in the next category -->
-    <p>for each category, change the expected response form on the patient page.</p>
+        <!-- save the doctor question and the response to the response form table in the database -->
+        <!-- when the doctor clicks the end interview button, the retrive the data from the response form table -->
 
-    <form action="patient-answers.php">
-        <input type="submit" name="end-interview" value="End Interview">
-    </form>
+        <!-- when that submit button is clicked, show the question on the parent portal   -->
+        <!-- have the form submit to patient.auth -->
+        <!-- the paitent auth should display the content that has been submitied and the paitnet should see the question -->
+        <!-- the paithent response form should show -->
+        <!-- the paitient response should be submitted and shown on the response section for the doctor -->
+
+
+
+        <!--the landing page for the paitent should be the page that asks for the code from the doctor to accept. that code should be saved as the session code  -->
+        <!-- so you update the session code so that the session for the patient and the doctor are the same -->
+
+        <!-- if the doctor clicks next category, then retrive the questions in the next category -->
+        <p>for each category, change the expected response form on the patient page.</p>
+
+        <form action="patient-answers.php">
+            <input type="submit" name="end-interview" value="End Interview">
+        </form>
+
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="js/index.js"></script>
