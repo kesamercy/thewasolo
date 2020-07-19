fb5952e1a62ca19589e92a56303b473403e97a65
Nekesa Mercy <kesamercy@gmail.com>
2020-07-19 17:48:21 -0400

    debugging to determine offset of ui elements

--- index-2.php
+++ index-2.php
@@ -48,7 +48,24 @@
 
         <hr class="w3-padding-64">
 
-        <?php include 'footer.php';?>
+        <footer>
+
+            <div class='w3-row'>
+                <div class='w3-third w3-center'>
+                    <p>
+                        Powered by <a href='https://www.w3schools.com/w3css/default.asp' target='_blank'>wandika</a>
+                    </p>
+                </div>
+                <div class='w3-third w3-center'>
+                    <p class='w3-center'> Copy right 2020</p>
+                    <div class='sharethis-inline-follow-buttons follow-buttons'></div>
+                </div>
+                <div class='w3-third w3-center'>
+                    <p>Contact Us</p>
+
+                </div>
+            </div>
+        </footer> ";
     </div>
 
     <script>
