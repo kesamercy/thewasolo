c28c35ec0b4dc2504705c82faf27789c4f26d57b
Nekesa Mercy <kesamercy@gmail.com>
2020-07-19 20:43:20 -0400

    modifiying the style sheets

--- index-2.php
+++ index-2.php
@@ -40,7 +40,7 @@ if (isset($_POST['btn-login'])) {
 <link
     href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap"
     rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="style-2.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
 
 <body>
 
