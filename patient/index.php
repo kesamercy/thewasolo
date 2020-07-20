ffdbbf7ac57d21e7dd8ba11b413a0832869ce5fc
Nekesa Mercy <kesamercy@gmail.com>
2020-07-20 12:22:42 -0400

    modifying ui elements for the patient index page

--- patient/index.php
+++ patient/index.php
@@ -40,27 +40,35 @@ if (isset($_POST['session-btn'])) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap"
+    rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="../style.css" />
 
 <body>
 
 
     <div class="content">
-    <?php include '../header.php';?>
+        <?php include '../header.php';?>
 
-    <h4>Patient Session</h4>
+        <div class="w3-center">
 
-    <p class="descritpion-text">
-        Enter your email the code retrived from the doctor session. This will allow your paitent to join your interview session.
-    </p>
+            <h4>Patient Session</h4>
 
-    <form method="POST" action="">
-    <input type="text" class="w3-input w3-border session-input" name="email" placeholder="Enter doctor email">
-        <input type="text" class="w3-input w3-border session-input" name="pat-code" placeholder="Enter the session code">
-        <input type="submit" class="w3-button w3-light-green" name="session-btn" value="Start Session" >
-    </form>
+            <p class="description-text">
+                Enter your email the code retrived from the doctor session. This will allow your paitent to join your
+                interview session.
+            </p>
 
+            <form method="POST" action="">
+                <input type="text" class="w3-input w3-border session-input" name="email"
+                    placeholder="Enter doctor email"> <br>
+                <input type="text" class="w3-input w3-border session-input" name="pat-code"
+                    placeholder="Enter the session code"> <br>
+                <input type="submit" class="w3-button w3-light-green" name="session-btn" value="Start Session">
+            </form>
+
+        </div>
     </div>
     <?php include '../footer.php';?>
     <script></script>
